cfbc16c4e0644ba50d4d6d2a6e7955925a4cc11a
Kristaps Svarinskis <kristapssvarinskisdp13@inbox.lv>
2021-04-20 14:39:49 +0300

    papildinats css un mazliet js

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,6 @@
-console.log("Strādā")
+let cart = document.querySelector('.cart');
+letinput = document.querySelectorAll('#input');
+
+window.addEventListener('keypress', key=>{
+    console.log(key.keyCode);
+});
